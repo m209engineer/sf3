3c984bbe520f0c1b595a8c9eb242e4a999dc173e
m209engineer <muhammadazizake66@gmail.com>
2025-09-23 15:45:29 +0500

    10021

--- src/app/Pages/system/system.ts
+++ src/app/Pages/system/system.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
-// last updated in 23.09.2025 14:26
+// last updated in 23.09.2025 15:43
 
 
 interface Student {
