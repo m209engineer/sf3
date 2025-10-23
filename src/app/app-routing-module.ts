4ada15a4ae1a76c85171ad617e6ac0e3fde089d9
m209engineer <muhammadazizake66@gmail.com>
2025-10-23 09:45:16 +0500

    10048

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -6,7 +6,7 @@ import { AuthGuard } from './Guards/auth.guard';
 import { Home } from './Pages/system/home/home';
 import { Rating } from './Pages/system/rating/rating';
 import { Profile } from './Pages/system/profile/profile';
-import { Market } from './Pages/system/market/market';
+
 
 
 
@@ -21,7 +21,7 @@ const routes: Routes = [
       { path: 'home', component: Home },
       { path: 'rating', component: Rating },
       { path: 'profile', component: Profile },
-      {path: 'market', component: Market},
+
       { path: '', redirectTo: 'home', pathMatch: 'full' }
     ]
   },
